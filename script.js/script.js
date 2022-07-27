ebd5d80f804000cf4b0e91e67aa1fd238e647027
Justin Franke <justinfranke23@gmail.com>
2022-07-27 07:06:05 -0400

    Completed contact us page

--- script.js/script.js
+++ script.js/script.js
@@ -10,3 +10,26 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", 
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }))
+
+/*Contact Form Validation*/
+
+const name = document.getElementById('name')
+const password = document.getElementById('password')
+const form = document.getElementById('form')
+const errorElement = document.getElementById('error')
+
+form.addEventListener('submit', (e) => {
+    let messages = []
+    if (name.value === '' || name.value == null){
+        messages.push('Name is required')
+    }
+
+    if (messages.length > 0) {
+        e.preventDefault()
+        errorElement.innerText = messages.join(', ')
+
+
+    }
+
+})
+
